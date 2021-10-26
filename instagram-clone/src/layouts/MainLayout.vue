f154b662edcac315fe3014ec6711ca0cda0133aa
imac_silver <imac_silver@imac-silverui-iMac.local>
2021-10-26 13:51:23 +0900

    first commit

---
+++ instagram-clone/src/layouts/MainLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+          <q-footer class="bg-white" bordered>
+            <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent">
+              <q-route-tab to="/" name="home" icon="mail" label="Home" />
+              <q-route-tab to="/camera" name="home" icon="alarm" label="Camera" /> 
+            </q-tabs>
+          </q-footer>
+
+        <q-toolbar-title>
+          Quasar App
+          </q-toolbar-title>
+
+          <div>Quasar v{{ $q.version }}</div>
+        </q-toolbar>
+      </q-header>
+
+   
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  data () {
+    return {
+
+    }
+  }
+})
+</script>
---
+++ instagram-clone/src/pages/PageHome.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h5>Home Page</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageHome'
+})
+</script>
