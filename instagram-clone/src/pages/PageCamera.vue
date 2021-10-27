dabbf34080e30b95edb0704e9796db7f6b8e8f58
imac_silver <imac_silver@imac-silverui-iMac.local>
2021-10-27 13:27:51 +0900

    update all

--- instagram-clone/src/pages/PageCamera.vue
+++ instagram-clone/src/pages/PageCamera.vue
@@ -1,10 +1,39 @@
 <template>
   <q-page class="constrains q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video src="" class="full-width" autoplay playsinline ref="video" />
+      <video
+        v-show="!imageCaptured"
+        src=""
+        class="full-width"
+        autoplay
+        playsinline
+        ref="video"
+      />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        calss="full-width"
+        height="240 "
+      />
     </div>
     <div class="text-center q-pa-md">
-      <q-btn color="grey-10" size="lg" icon="camera" round></q-btn>
+      <q-btn
+        v-if="hasCameraSupport"
+        color="grey-10"
+        size="lg"
+        icon="camera"
+        @click="captureImage"
+        round
+      />
+      <q-file
+        v-else
+        v-model="imageUpload"
+        @input="captureImageFallback"
+        icon="press"
+        outlined
+        label="Choose an image"
+        accept="image/*"
+      />
       <div class="row justify-center q-ma-md">
         <q-input
           v-model="post.caption"
@@ -37,6 +66,7 @@
 <script>
 import { defineComponent } from "vue";
 import { uid } from "quasar";
+require("md-gum-polyfill");
 
 export default defineComponent({
   name: "PageCamera",
@@ -49,6 +79,9 @@ export default defineComponent({
         photo: null,
         date: Date.now(),
       },
+      imageCaptured: false,
+      hasCameraSupport: true,
+      imageUpload: [],
     };
   },
   methods: {
@@ -59,11 +92,79 @@ export default defineComponent({
         })
         .then((stream) => {
           this.$refs.video.srcObject = stream;
+        })
+        .catch((error) => {
+          this.hasCameraSupport = false;
         });
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+      this.disableCamera();
+    },
+    captureImageFallback(file) {
+      this.post.photo = file;
+
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext("2d");
+
+      var reader = new FileReader();
+      reader.onload = (event) => {
+        var img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          this.imageCaptured = true;
+        };
+        img.src = event.target.result;
+      };
+      reader.readAsDataURL(file);
+      console.log(file);
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach((track) => {
+        track.stop();
+      });
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
     },
   },
   mounted() {
     this.initCamera();
+  },
+  beforeDestroy() {
+    if (this.hasCameraSupport) {
+      this.disableCamera();
+    }
   },
 });
 </script>
